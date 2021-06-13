486720effc8aeb589a00137d092d523117976afb
Adriano Kaito <adriano.kaito@gmail.com>
2021-06-13 17:42:44 -0300

    Bean Validation

--- src/main/java/com/ak/lancamentos/api/exceptionhandler/ResponseExceptionHandler.java
+++ src/main/java/com/ak/lancamentos/api/exceptionhandler/ResponseExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ak.lancamentos.api.exceptionhandler;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,6 +11,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -30,6 +34,26 @@ public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
 		List<Erro> erros = Arrays.asList(new Erro(msgUsuario, msgDesenvolvedor));
 		
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		List<Erro> erros = criarListaDeErros(ex.getBindingResult());
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	private List<Erro> criarListaDeErros(BindingResult bindingResult) {
+		List<Erro> erros = new ArrayList<>();
+		
+		for (FieldError fieldError : bindingResult.getFieldErrors()) {
+			String msgUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+			String msgDesenvolvedor = fieldError.toString();
+			erros.add(new Erro(msgUsuario, msgDesenvolvedor));
+		}
+		
+		return erros;
 	}
 	
 	// Classe auxiliar utilizada somente nesta classe
