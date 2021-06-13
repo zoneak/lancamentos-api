518d6e8bd6b51fc68d7c6eaa9af7abdcbb3805bc
Adriano Kaito <adriano.kaito@gmail.com>
2021-06-13 18:31:55 -0300

    Criação de Event de Recurso Criado - Location no HEADER

--- src/main/java/com/ak/lancamentos/api/resource/CategoriaResource.java
+++ src/main/java/com/ak/lancamentos/api/resource/CategoriaResource.java
@@ -1,12 +1,13 @@
 package com.ak.lancamentos.api.resource;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ak.lancamentos.api.event.RecursoCriadoEvent;
 import com.ak.lancamentos.api.model.Categoria;
 import com.ak.lancamentos.api.repository.CategoriaRepository;
 
@@ -26,6 +27,9 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping // Requisições GET caem aqui
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
@@ -35,12 +39,11 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
 		
-		// Retorna no HEADER da resposta a URI de como localizar o registro salvo
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
+		// Retorna no HEADER da resposta a URI de como localizar o registro salvo chamando o Event/Listener que faz isso
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
 		
 		// Retorna no BODY o registro salvo para já aparecer na tela, evitando ter que consultar para ver o registro salvo
-		return ResponseEntity.created(uri).body(categoriaSalva);
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
 	
 	@GetMapping("/{codigo}")
--- src/main/java/com/ak/lancamentos/api/resource/PessoaResource.java
+++ src/main/java/com/ak/lancamentos/api/resource/PessoaResource.java
@@ -1,12 +1,13 @@
 package com.ak.lancamentos.api.resource;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ak.lancamentos.api.event.RecursoCriadoEvent;
 import com.ak.lancamentos.api.model.Pessoa;
 import com.ak.lancamentos.api.repository.PessoaRepository;
 
@@ -26,6 +27,9 @@ public class PessoaResource {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public List<Pessoa> listar() {
 		return pessoaRepository.findAll(); 
@@ -35,12 +39,11 @@ public class PessoaResource {
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
 		
-		// Retorna no HEADER da resposta a URI de como localizar o registro salvo
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(pessoaSalva.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
+		// Retorna no HEADER da resposta a URI de como localizar o registro salvo chamando o Event/Listener que faz isso
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
 		
 		// Retorna no BODY o registro salvo para já aparecer na tela, evitando ter que consultar para ver o registro salvo
-		return ResponseEntity.created(uri).body(pessoaSalva);
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
 	
 	@GetMapping("/{codigo}")
