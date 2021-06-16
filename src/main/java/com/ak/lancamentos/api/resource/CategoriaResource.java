ae07fcd3a9f1f03f669f96f6fd24e54c05de9e18
Adriano Kaito <adriano.kaito@gmail.com>
2021-06-15 23:15:06 -0300

    CORS - Criada pagina de testagem de requisição

--- src/main/java/com/ak/lancamentos/api/resource/CategoriaResource.java
+++ src/main/java/com/ak/lancamentos/api/resource/CategoriaResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,8 @@ public class CategoriaResource {
 	@Autowired
 	private ApplicationEventPublisher publisher;
 	
+	// Apenas para testar o CORS - Não utilizado na app devido a conflito com spring security
+	//@CrossOrigin(maxAge = 10, origins = { "http://localhost:8000" })
 	@GetMapping // Requisições GET caem aqui
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
