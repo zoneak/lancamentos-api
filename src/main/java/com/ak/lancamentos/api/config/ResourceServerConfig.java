64c869db908f143568746150cb40c59440e2ba78
Adriano Kaito <adriano.kaito@gmail.com>
2021-06-16 11:22:37 -0300

    Movido usuarios de autenticação para banco de dados

--- src/main/java/com/ak/lancamentos/api/config/ResourceServerConfig.java
+++ src/main/java/com/ak/lancamentos/api/config/ResourceServerConfig.java
@@ -6,6 +6,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -16,11 +19,19 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+		
+		/* 
+		 * Utilizar com usuário na memória:
 		auth.inMemoryAuthentication()
 			.withUser("admin").password("{noop}admin").roles("ROLE"); // {noop} é necessário nesta versão do Spring para funcionar
+		*/	
 	}
-	
+
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
@@ -35,5 +46,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.stateless(true);
+	}
+	
+	private PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
